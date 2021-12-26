480d24f16494f76d4f3cea51db470eb83a93967a
sudomaaz <maazm0649@gmail.com>
2021-12-26 15:48:13 +0530

    Initial Boilerplate

--- src/config/config.js
+++ src/config/config.js
@@ -7,12 +7,12 @@ dotenv.config({ path: path.join(__dirname, '../../.env') });
 const envVarsSchema = Joi.object()
   .keys({
     NODE_ENV: Joi.string().valid('production', 'development').required(),
-    PORT: Joi.number().default(3000),
+    PORT: Joi.number().positive().default(3000),
     MONGODB_URL: Joi.string().required().description('Mongo DB url'),
   })
   .unknown();
 
-const { value: envVars, error } = envVarsSchema.validate(process.env);
+const { value: envVars, error } = envVarsSchema.prefs({ errors: { label: 'key' } }).validate(process.env);
 
 if (error) {
   throw new Error(`Config validation error: ${error.message}`);
